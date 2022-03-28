6a2eb739d4d8f56877dfe920a595e0d40000db93
SM24524 <56416969+SM24524@users.noreply.github.com>
2022-03-27 20:36:25 -0400

    Update E4.java

--- E4.java
+++ E4.java
@@ -1,5 +1,6 @@
 import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
 import java.io.*;
 
 
@@ -41,12 +42,46 @@ class E4{
    do{
       randomnum = rand.nextInt(11) + 2;
       write.println(randomnum);
-      System.out.println(randomnum);
       }while(randomnum != sum);
       
    out.close();
    
+   Scanner number = new Scanner(System.in);
    
+   int usernum;
+   do{
+   System.out.print("Enter a number between 0-10: ");
+   usernum = input.nextInt();
+      if (usernum < 0 || usernum > 10){
+      System.out.println("Invalid Input");
+         }
+      }while(usernum < 0 || usernum > 10);
+      
+   number.close();
    
+   Scanner reader = new Scanner(file);
+   
+   double vowels = 0;
+   
+   while(reader.hasNextLine()){
+   String line = reader.nextLine();
+   for (int i = 0; i <= line.length(); i++){
+   char ch = line.charAt(i);
+      if(ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o'|| ch == 'U' || ch == 'u'){
+         vowels = vowels + 1;
+            }
+      else if (ch == '!' || ch == '.' || ch == '?'){
+            break;
+            }
+         }
+
+   System.out.print(line.length());
+   System.out.print(" characters in the line. ");
+   double percent = (vowels/line.length())*100;
+   System.out.print((Math.round(percent * 100d) / 100d));
+   System.out.println("% are vowels.");
+   vowels = 0;
+   percent = 0;
+   }
    }
 }
